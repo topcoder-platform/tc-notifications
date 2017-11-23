b05d46cc7c7cf7a73c79006d355938ee3964b2b4
Nkumar <nagendrakumar.mruthyunja@topcoder.com>
2017-11-23 21:30:19 +0530

    fixed node js error

--- config/default.js
+++ config/default.js
@@ -34,3 +34,23 @@ module.exports = {
     },
   },
 
+// comma separated Kafka hosts
+  KAFKA_URL: process.env.KAFKA_URL, 
+
+  // ignore prefix for topics in the Kafka, e.g.
+  KAFKA_TOPIC_IGNORE_PREFIX: process.env.KAFKA_TOPIC_IGNORE_PREFIX,
+
+  // when notification server is deployed to multiple instances, the instances should use same group id so that
+  // Kafka event is not handled duplicately in the group, an event is handled by only one instance in the group
+  KAFKA_GROUP_ID: process.env.KAFKA_GROUP_ID,
+
+  // Kafka connection certificate, optional;
+  // if not provided, then SSL connection is not used, direct insecure connection is used;
+  // if provided, it can be either path to certificate file or certificate content
+  KAFKA_CLIENT_CERT: process.env.KAFKA_CLIENT_CERT,
+
+  // Kafka connection private key, optional;
+  // if not provided, then SSL connection is not used, direct insecure connection is used;
+  // if provided, it can be either path to private key file or private key content
+  KAFKA_CLIENT_CERT_KEY: process.env.KAFKA_CLIENT_CERT_KEY,
+};
