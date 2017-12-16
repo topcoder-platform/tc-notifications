ae71b37935d2f4d5fbf9bc9a02aa7a7711ad41fa
gondzo <gondzo@gmail.com>
2017-12-17 00:37:47 +0100

    update api path

--- connect/config.js
+++ connect/config.js
@@ -3,8 +3,8 @@
  */
 
 module.exports = {
-  TC_API_V3_BASE_URL: process.env.TC_API_V3_BASE_URL || 'https://api.topcoder-dev.com/v4',
-  TC_API_V4_BASE_URL: process.env.TC_API_V4_BASE_URL || 'https://api.topcoder-dev.com/v3',
+  TC_API_V3_BASE_URL: process.env.TC_API_V3_BASE_URL || 'https://api.topcoder-dev.com/v3',
+  TC_API_V4_BASE_URL: process.env.TC_API_V4_BASE_URL || 'https://api.topcoder-dev.com/v4',
   TC_ADMIN_TOKEN: process.env.TC_ADMIN_TOKEN,
 
   // Probably temporary variables for TopCoder role ids for 'Connect Manager', 'Connect Copilot' and 'administrator'
