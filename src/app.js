406040a99a2805fe79090a9deb3abed9f8eba3ca
gondzo <gondzo@gmail.com>
2018-01-11 19:12:54 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -52,7 +52,7 @@ function startKafkaConsumer(handlers) {
       // save notifications
       .then((notifications) => Promise.all(_.map(notifications, (notification) => models.Notification.create({
         userId: notification.userId,
-        type: notification.newType | topicName,
+        type: notification.newType || topicName,
         version: notification.version || null,
         contents: _.extend({}, messageJSON, notification.contents),
         read: false,
