d0a0dfed70a2ca984d0cdd12289bdd40084db897
gondzo <gondzo@gmail.com>
2018-03-18 19:29:17 +0100

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -18,5 +18,5 @@ const NotificationSetting = require('./NotificationSetting')(sequelize, DataType
 module.exports = {
   Notification,
   NotificationSetting,
-  init: () => sequelize.sync(),
+  init: () => sequelize.sync({ alter: true }),
 };
