41e37cc5b5a57a76e536bc82677f82fccbbea12a
Vikas Agarwal <vikas.agarwal@topcoder.com>
2018-04-04 16:31:15 +0530

    debugging

--- src/services/BusAPI.js
+++ src/services/BusAPI.js
@@ -9,7 +9,10 @@ const _ = require('lodash');
  *
  * @return {Promise}          promise resolved to post event
  */
-const postEvent = (event) => request
+const postEvent = (event) => {
+  console.log('URL: ', `${config.TC_API_BASE_URL}/eventbus/events`);
+  console.log('URL: ', `${config.TC_API_V3_BASE_URL}/eventbus/events`);
+  return request
   .post(`${config.TC_API_BASE_URL}/eventbus/events`)
   .set('Content-Type', 'application/json')
   .set('Authorization', `Bearer ${config.BUS_API_AUTH_TOKEN}`)
@@ -22,6 +25,7 @@ const postEvent = (event) => request
       (errorDetails ? ' Server response: ' + errorDetails : '')
     );
   });
+}
 
 module.exports = {
   postEvent,
