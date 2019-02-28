e7423294ac3e219f792bb379e8b7a973a62492a3
gondzo <gondzo@gmail.com>
2019-02-28 19:29:30 +0100

    Feature/roles cache (#91)
    * fetch roles from api and cache
    
    * deploy feature branch
    
    * remove feature branch from deployment

--- connect/events-config.js
+++ connect/events-config.js
@@ -1,7 +1,6 @@
 /**
  * Configuration of connect events
  */
-const config = require('./config');
 const { BUS_API_EVENT } = require('./constants');
 
 // project member role names
@@ -26,15 +25,6 @@ const ROLE_CONNECT_MANAGER = 'Connect Manager';
 const ROLE_CONNECT_COPILOT_MANAGER = 'Connect Copilot Manager';
 const ROLE_CONNECT_ACCOUNT_MANAGER = 'Connect Account Manager';
 const ROLE_ADMINISTRATOR = 'administrator';
-
-// TopCoder role rules
-const TOPCODER_ROLE_RULES = {
-  [ROLE_CONNECT_COPILOT]: { id: config.CONNECT_COPILOT_ROLE_ID },
-  [ROLE_CONNECT_MANAGER]: { id: config.CONNECT_MANAGER_ROLE_ID },
-  [ROLE_CONNECT_COPILOT_MANAGER]: { id: config.CONNECT_COPILOT_MANAGER_ROLE_ID },
-  [ROLE_CONNECT_ACCOUNT_MANAGER]: { id: config.CONNECT_ACCOUNT_MANAGER_ROLE_ID },
-  [ROLE_ADMINISTRATOR]: { id: config.ADMINISTRATOR_ROLE_ID },
-};
 
 /**
  * Supported events configuration
@@ -313,7 +303,6 @@ const EVENT_BUNDLES = {
 
 module.exports = {
   PROJECT_ROLE_RULES,
-  TOPCODER_ROLE_RULES,
   EVENTS,
   EVENT_BUNDLES,
 
