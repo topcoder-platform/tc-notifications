9c9d8b7b3a1b664541f4e903583b7b5d9e7b7d3a
nkumar-topcoder <33625707+nkumar-topcoder@users.noreply.github.com>
2019-03-22 17:37:38 +0530

    Update build.sh
    [skip ci]

--- build.sh
+++ build.sh
@@ -5,17 +5,17 @@ set -eo pipefail
 # This script expects a single argument: NODE_ENV, which must be either
 # "development" or "production".
 
-NODE_ENV=$1
+# NODE_ENV=$1
 
-ENV=$1
-AWS_REGION=$(eval "echo \$${ENV}_AWS_REGION")
-AWS_ACCESS_KEY_ID=$(eval "echo \$${ENV}_AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY=$(eval "echo \$${ENV}_AWS_SECRET_ACCESS_KEY")
-AWS_ACCOUNT_ID=$(eval "echo \$${ENV}_AWS_ACCOUNT_ID")
-AWS_REPOSITORY=$(eval "echo \$${ENV}_AWS_REPOSITORY") 
+#ENV=$1
+#AWS_REGION=$(eval "echo \$${ENV}_AWS_REGION")
+#AWS_ACCESS_KEY_ID=$(eval "echo \$${ENV}_AWS_ACCESS_KEY_ID")
+#AWS_SECRET_ACCESS_KEY=$(eval "echo \$${ENV}_AWS_SECRET_ACCESS_KEY")
+#AWS_ACCOUNT_ID=$(eval "echo \$${ENV}_AWS_ACCOUNT_ID")
+#AWS_REPOSITORY=$(eval "echo \$${ENV}_AWS_REPOSITORY") 
 
-TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/tc-notifications:$CIRCLE_SHA1
-
+#TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/tc-notifications:$CIRCLE_SHA1
+TAG="tc-notifications:latest"
 docker build -t $TAG .
 
 # Copies "node_modules" from the created image, if necessary for caching.
