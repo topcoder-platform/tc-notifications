f17135bca710733dc3496046d6f3f3b31fbda853
Sachin Maheshwari <sachin.maheshwari@topcoder.com>
2019-04-30 16:48:49 +0530

    adding 'Challenge checkpoint review' support

--- src/processors/index.js
+++ src/processors/index.js
@@ -6,10 +6,12 @@
 const ChallengeCreatedHandler = require('./challenge/ChallengeCreatedHandler');
 const ChallengePhaseWarningHandler = require('./challenge/ChallengePhaseWarningHandler');
 const ChallengeHandler = require('./challenge/ChallengeHandler');
+const AutoPilotHandler = require('./challenge/AutoPilotHandler')
 
 // Exports
 module.exports = {
   handleChallengeCreated: ChallengeCreatedHandler.handle,
   handleChallengePhaseWarning: ChallengePhaseWarningHandler.handle,
   handleChallenge: ChallengeHandler.handle,
+  handleAutoPilot: AutoPilotHandler.handle,
 };
---
+++ src/services/AutoPilotService.js
@@ -0,0 +1,53 @@
+/**
+ * Autopilot general handler service.
+ */
+
+'use strict';
+
+const joi = require('joi');
+const _ = require('lodash')
+const logger = require('../common/logger');
+const tcApiHelper = require('../common/tcApiHelper');
+
+/**
+ * Handle autopilot message
+ * @param {Object} message the Kafka message
+ * @param {Object} ruleSets 
+ * @returns {Array} the notifications
+ */
+function* handle(message, ruleSets) {
+
+  if ((message.payload.phaseTypeName === _.get(ruleSets, "phaseTypeName"))
+    && (message.payload.state === _.get(ruleSets, "state"))) {
+    const challengeId = message.payload.projectId
+    const usersInfo = yield tcApiHelper.getUsersInfoFromChallenge(challengeId)
+    const filerOnRoles = _.get(ruleSets, "roles")
+    const users = tcApiHelper.filterChallengeUsers(usersInfo, filerOnRoles)
+    logger.info(`Successfully filetered ${users.length} users on rulesets ${JSON.stringify(filerOnRoles)} `)
+    // notify users of message
+    return yield tcApiHelper.notifyUsersOfMessage(users, message);
+  }
+  return {}
+}
+
+handle.schema = {
+  message: joi.object().keys({
+    topic: joi.string().required(),
+    originator: joi.string().required(),
+    timestamp: joi.date().required(),
+    'mime-type': joi.string().required(),
+    payload: joi.object().keys({
+      phaseTypeName: joi.string().required(),
+      state: joi.string().required(),
+      projectId: joi.number().integer().min(1)
+    }).unknown(true).required(),
+  }).required(),
+  ruleSets: joi.object()
+}
+
+// Exports
+module.exports = {
+  handle,
+}
+
+logger.buildService(module.exports);
