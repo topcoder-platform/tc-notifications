f991dd85f5469ec075a0126e5f10f031a1254d88
Sachin Maheshwari <sachin.maheshwari@topcoder.com>
2020-06-03 19:50:19 +0530

    Manually corrected duplicate broadcast rows in database with these SQLs.

---
+++ migrations/v2.0.4.sql
@@ -0,0 +1,29 @@
+
+-- query to identify the duplicate rows in bulk_message_user_refs table
+SELECT count(*), bulk_message_id, user_id  FROM "bulk_message_user_refs" 
+   GROUP BY bulk_message_id, user_id HAVING count(*) > 1;
+
+-- create temp table and store duplicate broadcast notification rows
+SELECT * INTO temptable FROM "Notifications" WHERE id IN  
+   (
+       SELECT a.notification_id FROM "bulk_message_user_refs" AS "a", 
+       "bulk_message_user_refs" AS "b" 
+       WHERE a.id < b.id 
+       AND a.bulk_message_id = b.bulk_message_id 
+       AND a.user_id = b.user_id
+    );
+
+-- DELETE duplicate rows from bulk_message_user_refs table
+DELETE FROM "bulk_message_user_refs" AS "a" 
+  USING "bulk_message_user_refs" AS "b" 
+  WHERE a.id < b.id 
+  AND a.bulk_message_id = b.bulk_message_id 
+  AND a.user_id = b.user_id;
+
+-- DELETE duplicate rows from Notifications
+DELETE FROM "Notifications" 
+WHERE id IN (SELECT id FROM temptable) 
+AND type = 'admin.notification.broadcast';
+
+-- make unique column to avoid duplicate insert 
+ALTER TABLE bulk_message_user_refs ADD UNIQUE (bulk_message_id, user_id);
